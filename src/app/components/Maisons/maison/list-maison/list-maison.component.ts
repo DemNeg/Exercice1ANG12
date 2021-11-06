b14494d980cade92b45f15e1fcb4b73c91eb93cd
singou <singoukdembele@gmal.con>
2021-11-06 20:01:22 +0100

    My second commit with decomposition

---
+++ src/app/components/Maisons/maison/list-maison/list-maison.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-maison',
+  templateUrl: './list-maison.component.html',
+  styleUrls: ['./list-maison.component.css']
+})
+export class ListMaisonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
